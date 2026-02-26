7730500188a558cfa43a38a91cd16220e87da447
teerapat-ch <coptee413@gmail.com>
2026-02-26 16:22:13 +0700

    Update main_project.ino

--- main_project/main_project.ino
+++ main_project/main_project.ino
@@ -3,37 +3,50 @@
 
 #define LoadDT  21   // ขา DT ต่อกับ GPIO 21
 #define LoadSCK 22   // ขา SCK ต่อกับ GPIO 22
-#define servoPin 10
+#define servoPin 10 // Motor
+#define buzzPin 2  // Buzzer
 
 HX711 scale;
 Servo myservo;
 
-unsigned long lastSec = 0;
+ฺbool activated = true; // เปิดการให้อาหารอัติโนมัติไหม (เดี๋ยวคุยอีกทีว่าจะมีมั้ย)
+
+unsigned long interval = 1000; // วัดน้ำหนักทุกๆ X วินาที
+unsigned long lastSec = 0; // ไว้เช็คเวลาแทน delay()
+
+void fillFood() {
+  tone(buzzPin, 1000);
+
+  // Motor code
+  // ...
+
+  delay(1000);
+}
 
 void setup() {
   Serial.begin(115200);
 
+  // Load cell setup
   scale.begin(LoadDT, LoadSCK);
   Serial.println("Initializing the scale...");
   scale.set_scale();     // ตั้งค่าเริ่มต้น
   scale.tare();          // รีเซ็ตค่าน้ำหนักเป็น 0
   Serial.println("Scale ready");
 
+  // Servo setup
   myservo.attach(servoPin);
 }
 
 void loop() {
-
   unsigned long currSec = millis();
-
   if (currSec - lastSec >= 1000) {
+    lastSec = currSec;
+    float w = scale.get_units(10);
     Serial.print("Weight: ");
-    Serial.print(scale.get_units(10), 2);  // อ่านค่าเฉลี่ย 10 ครั้ง
-    Serial.println(" g");
+    Serial.println(w);
 
-    if (scale.get_units(10) < 1) {
-      myservo.writeMicroseconds(0);
-      delay(1000);
+    if (currentWeight < 5.0) {
+      fillFood();
     }
   }
 }
